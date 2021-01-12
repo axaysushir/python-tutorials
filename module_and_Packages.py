fb98d5aacb0df6171e26cb76c8676a9346915454
axaysushir <akshay.sushir99@gmail.com>
2021-01-12 20:17:56 +0530

    modules

---
+++ module_and_Packages.py
@@ -0,0 +1,18 @@
+# n programming, a module is a piece of software that has a specific functionality. 
+# For example, when building a ping pong game, one module would be responsible for the 
+# game logic, and another module would be responsible for drawing the game on the screen. 
+# Each module is a different file, which can be edited separately.
+
+# There is lots of built in modules are available in python and we can imprort it
+# directly in our code
+# Modules in Python are simply Python files with a .py extension. The name of the module will 
+# be the name of the file. A Python module can have a set of functions, classes or variables defined and implemented.
+
+# for example 
+from datetime import timezone
+
+# this import statatement import timezone form datetime modules
+
+import numpy as np
+
+# this import numpy package in our code and we can use this package any where in our code locally.
